df031120b4d790d015a0546390b347d8c7f04b98
hoaglee666 <nghimegamede55@gmail.com>
2024-02-21 08:40:51 +0700

    second

--- project1/project1/Form1.cs
+++ project1/project1/Form1.cs
@@ -2,9 +2,15 @@ namespace project1
 {
     public partial class Form1 : Form
     {
+        // edit#1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
